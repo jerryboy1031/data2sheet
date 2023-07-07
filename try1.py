445f52a1576d235782be5a225f046c787a0eeb51
jerryboy1031 <jerryboy10311@gmail.com>
2023-07-07 15:29:47 +0800

    deleted:    main.py 	modified:   try1.py

--- try1.py
+++ try1.py
@@ -4,20 +4,22 @@ from google.oauth2.service_account import Credentials
 
 #launches a browser asking you for authentication
 def try1():
-    scope = ['https://www.googleapis.com/auth/spreadsheets']#, 'https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.file']
+    scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.file']
     #credentials = ServiceAccountCredentials.from_json_keyfile_name(mykey_json, scope)
     credentials= Credentials.from_service_account_file("secret_credential.json", scopes=scope)
     
     client = gspread.authorize(credentials)
 
-    sh = client.open('EU tour') #error--------------
+    sh = client.open(title='EU_tour')#,folder_id='1CCJ6d-P381whToCFP6V9rb_mkI84GuHIF6z5rqWAzzg') #error--------------
     
     #gc= gspread.oauth(credentials_filename=r'C:/Users/User/anaconda3/Lib\site-packages/gspread/credential20230705.json') 
     #print(sh.get('總覽!B2'))
+    
     wks= sh.worksheet("總覽")
+    wks.insert_row([1000],2)
     print(wks.get_all_records())
     
-    wks.insert_row("hello",2)
+    
     return 0
 
 
